de7e2ee86fe6b1fb9579051f0ec2cd07287118cc
tori-takashi <tori.toutoshi@gmail.com>
2018-11-06 18:18:43 +0900

    更新をできるようにした

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,12 +8,21 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
   def show
     @event = Event.find(params[:id])
     set_response
   end
 
   def update
+    @event = Event.find(params[:id])
+    update_data = params.require(:event).permit(:name, :held_at)
+    if @event.update!(update_data)
+      redirect_to events_path, notice: "更新しました"
+    end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post '/event/create' => "events#create", as: :create_event
   get '/event/new_event' => "events#new", as: :new_event
   delete '/event/:id' => "events#delete", as: :delete_event
+  patch '/event/:id' => "events#update", as: :update_event
+  get '/event/edit/:id' => "events#edit", as: :edit_event
   
   get "/user/:id" => "users#show"
 
