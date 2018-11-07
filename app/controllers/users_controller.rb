f80b76104d6883d1a1875526e91c3101f1c5da47
tori-takashi <tori.toutoshi@gmail.com>
2018-11-07 13:48:51 +0900

    ユーザーのコントローラを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,48 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    set_response
+  end
+
   def show
     @user = User.find(params[:id])
+    set_response
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    update_data = user_params
+    if @user.update(update_data)
+      redirect_to users_path
+    end
+  end 
+
+  private
+  def set_response
     respond_to do |format|
       format.html
       format.json
     end
   end
+
+  def user_params
+    params.require(:user).permit(:name, :display_name, :self_introduction)
+  end
+
 end
