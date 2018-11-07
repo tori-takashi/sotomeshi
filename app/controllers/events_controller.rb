2666e103463d51770ff8e930e1115183c279ff2a
tori-takashi <tori.toutoshi@gmail.com>
2018-11-07 12:39:34 +0900

    リファクタリング

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,45 +3,46 @@ class EventsController < ApplicationController
     @events = Event.all
     set_response
   end
+
+  def show
+    @event = Event.find(params[:id])
+    set_response
+  end
   
   def new
     @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params.require(:event).permit(:name, :held_at))
+    if @event.save
+      redirect_to events_path
+    else
+      render :create_event
+     end
   end
 
   def edit
     @event = Event.find(params[:id])
   end
 
-  def show
-    @event = Event.find(params[:id])
-    set_response
-  end
-
   def update
     @event = Event.find(params[:id])
     update_data = params.require(:event).permit(:name, :held_at)
     if @event.update!(update_data)
-      redirect_to events_path, notice: "更新しました"
+      redirect_to events_path
     end
   end
 
-  def create
-    @event = Event.new(params.require(:event).permit(:name, :held_at))
-    if @event.save
-      redirect_to events_path, notice: "作成しました"
-    else
-      render :create_event
-     end
-  end
-
-  def delete
+  def destroy
     @event = Event.find(params[:id])
     if @event.destroy!
-      redirect_to events_path, notice: "削除しました"
+      redirect_to events_path
     end
   end
 
   private
+  #responseだと別メソッドをオーバーライドしてしまう
   def set_response
     respond_to do |f|
       f.html
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+
   root to: "landing#index"
 
-  get '/events' => "events#index"
-  get '/events/:id' => "events#show"
-  post '/event/create' => "events#create", as: :create_event
-  get '/event/new_event' => "events#new", as: :new_event
-  delete '/event/:id' => "events#delete", as: :delete_event
-  patch '/event/:id' => "events#update", as: :update_event
-  get '/event/edit/:id' => "events#edit", as: :edit_event
+  #get '/events' => "events#index"
+  #get '/events/:id' => "events#show"
+  #post '/event/create' => "events#create", as: :create_event
+  #get '/event/new_event' => "events#new", as: :new_event
+  #delete '/event/:id' => "events#delete", as: :delete_event
+  #patch '/event/:id' => "events#update", as: :update_event
+  #get '/event/edit/:id' => "events#edit", as: :edit_event
+
+  resources :events
   
   get "/user/:id" => "users#show"
 
